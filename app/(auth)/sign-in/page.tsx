95741dd109475944ecb1f8f4e9968af9ca484607
uma1123 <yuma1596@gmail.com>
2026-02-24 23:47:25 +0900

    feat: サインイン、サインアップ、ブックマーク、設定ページを追加した
    - サインインページを新規作成
    - サインアップページを新規作成
    - ブックマークページを新規作成
    - 設定ページを新規作成
    - Supabaseミドルウェアを追加
    - Supabaseクライアント作成機能を追加

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function signInPage() {
+  return <div>サインインページ</div>;
+}
---
+++ app/(tabs)/settings/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SettingPage() {
+  return <div>設定ページ</div>;
+}
